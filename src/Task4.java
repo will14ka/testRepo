31c8eac4dfd0dbeb99b0cb885a13f5953bc4a41e
will14ka <kateryna.ilchuk@gmail.com>
2018-08-21 13:55:45 +0300

    Task-2-2 #3

--- src/Task4.java
+++ src/Task4.java
@@ -5,7 +5,7 @@ public class Task4 {
     public static void main (String[] args) {
 
         System.out.print("Enter massive from 10 elements, each element should be a number \n");
-        System.out.print("Ex. 9 5 6 4 3 1 2 7 8 10 \n");
+        System.out.print("Ex. 9 5 16 42 3 1 2 7 8 10 \n");
         Scanner element = new Scanner (System.in);
         int[] a = new int [10];
         a[0]=element.nextInt();
