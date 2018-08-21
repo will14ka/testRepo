25a55bedb5e04d96ad86ce4086ee4cc79ab4fcd5
will14ka <kateryna.ilchuk@gmail.com>
2018-08-21 12:56:09 +0300

    Task-2-2 #3

--- src/Task3.java
+++ src/Task3.java
@@ -9,10 +9,16 @@ public class Task3 {
         System.out.print("Please enter number2:");
         Scanner n2 = new Scanner (System.in);
         int number2 = Integer.parseInt(n2.nextLine());
+        System.out.print("Simple operations results: \n");
+        System.out.print("number1+number2=");
         System.out.println(number1+number2);
+        System.out.print("number2-number1=");
         System.out.println(number2-number1);
+        System.out.print("number2/number1=");
         System.out.println(number2/number1);
+        System.out.print("number2*number2=");
         System.out.println(number2*number2);
+        System.out.print("(number1+number2)%number1=");
         System.out.println((number1+number2)%number1);
 
     }
