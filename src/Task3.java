46e662a64e19a6414d13fb2876b677f42a348616
will14ka <kateryna.ilchuk@gmail.com>
2018-08-21 12:42:29 +0300

    Task-2-2 #3

---
+++ src/Task3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Task3 {
+    public static void main (String[] args) {
+
+        System.out.print("Please enter number1:");
+        Scanner n1 = new Scanner (System.in);
+        int number1 = Integer.parseInt(n1.nextLine());
+        System.out.print("Please enter number2:");
+        Scanner n2 = new Scanner (System.in);
+        int number2 = Integer.parseInt(n2.nextLine());
+        System.out.println(number1+number2);
+        System.out.println(number2-number1);
+        System.out.println(number2/number1);
+        System.out.println(number2*number2);
+        System.out.println((number1+number2)%number1);
+
+    }
+}
