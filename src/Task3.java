c77def607d6a7e826a1006fd6dfdb237da3d69c9
will14ka <kateryna.ilchuk@gmail.com>
2018-08-23 16:10:33 +0300

    Task-2-2 #3 bug fixing (removed extra scaner)

--- src/Task3.java
+++ src/Task3.java
@@ -4,11 +4,10 @@ public class Task3 {
     public static void main (String[] args) {
 
         System.out.print("Please enter number1:");
-        Scanner n1 = new Scanner (System.in);
-        int number1 = Integer.parseInt(n1.nextLine());
+        Scanner n = new Scanner (System.in);
+        int number1 = Integer.parseInt(n.nextLine());
         System.out.print("Please enter number2:");
-        Scanner n2 = new Scanner (System.in);
-        int number2 = Integer.parseInt(n2.nextLine());
+        int number2 = Integer.parseInt(n.nextLine());
         System.out.print("Simple operations results: \n");
         System.out.print("number1+number2=");
         System.out.println(number1+number2);
