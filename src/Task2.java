44ca2741a3654141488db906fd16d5e6493c49f8
will14ka <kateryna.ilchuk@gmail.com>
2018-08-23 15:14:37 +0300

    Task-2-2 #3 bug fix (multi scan creation is fixed)

--- src/Task2.java
+++ src/Task2.java
@@ -4,15 +4,13 @@ public class Task2 {
     public static void main (String[] args) {
 
         System.out.print("Please enter any data to String 1:");
-        Scanner str1 = new Scanner (System.in);
-        String string1 = str1.nextLine();
+        Scanner str = new Scanner (System.in);
+        String string1 = str.nextLine();
         System.out.print("Please enter any data to String 2:");
-        Scanner str2 = new Scanner (System.in);
-        String string2 = str2.nextLine();
+        String string2 = str.nextLine();
         System.out.print("Please enter any data to String 3:");
-        Scanner str3 = new Scanner (System.in);
-        String string3 = str3.nextLine();
-       System.out.println(string1+string3);
+        String string3 = str.nextLine();
+        System.out.println(string1+string3);
         System.out.println(string3+string2+string1);
         System.out.println(string1+string2+string3);
 
