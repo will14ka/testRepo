cb3a2bdf7cd0c5d7e5c8291debcb8232b7915178
will14ka <kateryna.ilchuk@gmail.com>
2018-08-24 18:28:37 +0300

    Task-3-2 #5

---
+++ src/Task5.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+
+public class Task5 {
+    public static void main (String[] args) {
+
+        int[] massive = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+        int x = massive[massive.length-1];
+        massive[massive.length-1]=massive[0];
+        massive[0]=x;
+        System.out.println(Arrays.toString(massive));
+    }
+}
