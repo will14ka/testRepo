06c01256c46aba43bb16293d1bc00a9b999e3591
will14ka <kateryna.ilchuk@gmail.com>
2018-08-24 18:43:41 +0300

    Task-2-2 #3 bug fixing (removed extra scaner)

--- src/Task3.java
+++ src/Task3.java
@@ -3,22 +3,17 @@ import java.util.Scanner;
 public class Task3 {
     public static void main (String[] args) {
 
-        System.out.print("Please enter number1:");
+        System.out.print("Please enter X:");
         Scanner n = new Scanner (System.in);
         int number1 = Integer.parseInt(n.nextLine());
-        System.out.print("Please enter number2:");
+        System.out.print("Please enter Y:");
         int number2 = Integer.parseInt(n.nextLine());
-        System.out.print("Simple operations results: \n");
-        System.out.print("number1+number2=");
-        System.out.println(number1+number2);
-        System.out.print("number2-number1=");
-        System.out.println(number2-number1);
-        System.out.print("number2/number1=");
-        System.out.println(number2/number1);
-        System.out.print("number2*number2=");
-        System.out.println(number2*number2);
-        System.out.print("(number1+number2)%number1=");
-        System.out.println((number1+number2)%number1);
 
+        System.out.print("Simple operations results: \n");
+        System.out.print("X+Y="+(number1+number2)+"\n");
+        System.out.print("Y-X="+(number2-number1)+"\n");
+        System.out.print("Y/X="+(number2/number1)+"\n");
+        System.out.print("Y*Y="+(number2*number2)+"\n");
+        System.out.print("(X+Y)%X="+((number1+number2)%number1)+"\n");
     }
 }
