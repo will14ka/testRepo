05f7ed4ef31b4758d103cfe4f7e2baf6cac2ffca
will14ka <kateryna.ilchuk@gmail.com>
2018-08-27 01:10:14 +0300

    Task-4-3 #10

---
+++ src/Task10.java
@@ -0,0 +1,10 @@
+public class Task10 {
+    public static void main(String... args ){
+        for (int i=0; i <= 100; i++){
+           int[] array=new int [101];
+           array[i]=(100-i);
+           //System.out.println(array[i]);
+        }
+    }
+
+}
