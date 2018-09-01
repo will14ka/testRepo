a647513480b12d2e02f0efd16fb596f1d68c8f47
will14ka <kateryna.ilchuk@gmail.com>
2018-09-01 12:30:23 +0300

    Task-3-4 #7

--- src/Task2.java
+++ src/Task2.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
 
 public class Task2 {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
         System.out.print("Please enter any data to String 1:");
-        Scanner str = new Scanner (System.in);
+        Scanner str = new Scanner(System.in);
         String string1 = str.nextLine();
         System.out.print("Please enter any data to String 2:");
         String string2 = str.nextLine();
         System.out.print("Please enter any data to String 3:");
         String string3 = str.nextLine();
-        System.out.println(string1+string3);
-        System.out.println(string3+string2+string1);
-        System.out.println(string1+string2+string3);
+        System.out.println(string1 + string3);
+        System.out.println(string3 + string2 + string1);
+        System.out.println(string1 + string2 + string3);
 
     }
 }
