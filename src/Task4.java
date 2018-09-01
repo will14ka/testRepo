302f8b141f53d4bca8c94c80ed4e508516cb3673
will14ka <kateryna.ilchuk@gmail.com>
2018-09-01 12:31:03 +0300

    Task-3-4 #7

--- src/Task4.java
+++ src/Task4.java
@@ -1,9 +1,9 @@
 import java.util.Arrays;
 
 public class Task4 {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
-        int[] numbers ={23456, 434, 3, 343, 0, 34, 5, 2, 2,10};
+        int[] numbers = {23456, 434, 3, 343, 0, 34, 5, 2, 2, 10};
 
         Arrays.sort(numbers);
         System.out.println(Arrays.toString(numbers));
