31a9170097090c7beb3d74f3e35ff405a8b81342
will14ka <kateryna.ilchuk@gmail.com>
2018-09-01 12:26:48 +0300

    Task-3-4 #7 re-wrote to using %

--- src/Task9.java
+++ src/Task9.java
@@ -1,7 +1,14 @@
 public class Task9 {
     public static void main(String... args) {
-        for (int i = 3; i <= 100; i += 3) {
-            System.out.println(i);
+        for (int i = 1; i < 101; i ++) {
+            {
+                if (i % 3 == 0) {
+                    System.out.println(i);
+                }
+                else {
+
+                }
+            }
         }
     }
 }
