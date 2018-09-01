07e417fe989766c32014965ba072fb223ac528ba
will14ka <kateryna.ilchuk@gmail.com>
2018-09-01 12:14:55 +0300

    fixes

--- src/Task10.java
+++ src/Task10.java
@@ -1,10 +1,11 @@
-public class Task10 {
-    public static void main(String... args ){
-        for (int i=0; i <= 100; i++){
-           int[] array=new int [101];
-           array[i]=(100-i);
-           //System.out.println(array[i]);
-        }
-    }
+import java.util.Arrays;
 
+public class Task10 {
+    public static void main(String... args) {
+        int[] array = new int[101];
+        for (int i = 0; i <= 100; i++) {
+            array[i] = (100 - i);
+        }
+      //  System.out.println(Arrays.toString(array));
+    }
 }
